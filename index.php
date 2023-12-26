1093870f11a0ffbaa50c5aa4834e525b2859dc30
Julien Wajsberg <felash@gmail.com>
2023-12-26 22:34:19 +0100

    Use a simpler doctype

--- index.php
+++ index.php
@@ -157,8 +157,7 @@ $Textual = new Calendar_Util_Textual();
 /* unicode à cause de mysql 4 */
 header('Content-Type: text/html; charset=utf-8');
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!DOCTYPE html>
 <html>
 <head>
 <title><?php echo $calendrier->getName() ?></title>
